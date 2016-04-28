bf9fbfe2ca66213a78dcaf8da59da4be7301daca
Cvetan Mihaylov <cvetan.mihaylov@gmail.com>
2016-04-28 17:48:12 +0300

    modify index.php logic, router class, and controller abstract class

---
+++ www-oop/application/registerController.class.php
@@ -0,0 +1,25 @@
+<?php
+
+class registerController extends abstractController
+{
+    public function index()
+    {
+        return 'Register Index';
+    }
+}
+
+class standingsController extends abstractController
+{
+    public function index()
+    {
+        return 'Standings Index';
+    }
+}
+
+class standingsController extends abstractController
+{
+    public function index()
+    {
+        return 'Standings Index';
+    }
+}
