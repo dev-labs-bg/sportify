7bbfef4848d1aa1288d9b8abb0c64a475b92c086
Cvetan Mihaylov <cvetan.mihaylov@gmail.com>
2016-05-04 19:49:26 +0300

    add body to add() method

--- www-oop/application/User.class.php
+++ www-oop/application/User.class.php
@@ -9,13 +9,20 @@ class User
     public $lastName;
     public $email;
     public $password;
+    public $passwordConfirm;
+    public $passwordHash;
 
     public function add()
     {
-
+        $result = $GLOBALS['db']->query(
+            "INSERT IGNORE INTO users(first_name,last_name,email,password_hash)
+                VALUES(:first_name, :last_name, :email, :password_hash)",
+            array('first_name' => $this->firstName, 'last_name' => $this->lastName, 'email' => $this->email,
+                'password_hash' => password_hash($this->password, PASSWORD_DEFAULT))
+        );
     }
 
-    public function modify()
+    public function change()
     {
 
     }
