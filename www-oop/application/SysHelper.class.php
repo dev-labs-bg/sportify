a5fe529234cc97a0c3df4616d4e5d9c59cca028b
Cvetan Mihaylov <cvetan.mihaylov@gmail.com>
2016-05-05 18:19:23 +0300

    move methods getHomeUrl(), getSiteUrl(), getStaticUrl(), getCssUrl(), getJsUrl(), getImgUrl() from base.php to SysHelper class

--- www-oop/application/SysHelper.class.php
+++ www-oop/application/SysHelper.class.php
@@ -110,4 +110,64 @@ class SysHelper
             ? date("Y-m-d", strtotime($var_date_to) + $sec_offset)
             : date("Y-m-d", time() + 1209600);
     }
+
+    /**
+     * Get base host url
+     *
+     * @return string
+     */
+    public static function getHomeUrl()
+    {
+        return 'http://' . $_SERVER['HTTP_HOST'];
+    }
+
+    /**
+     * Get full site url
+     *
+     * @return string
+     */
+    public static function getSiteUrl()
+    {
+        return self::getHomeUrl() . $_SERVER['REQUEST_URI'];
+    }
+
+    /**
+     * Get static dir url
+     *
+     * @return string
+     */
+    public static function getStaticUrl()
+    {
+        return self::getHomeUrl() . '/static/';
+    }
+
+    /**
+     * Get css dir url
+     *
+     * @return string
+     */
+    public static function getCssUrl()
+    {
+        return self::getStaticUrl() . 'css/';
+    }
+
+    /**
+     * Get js dir url
+     *
+     * @return string
+     */
+    public static function getJsUrl()
+    {
+        return self::getStaticUrl() . 'js/';
+    }
+
+    /**
+     * Get img dir url
+     *
+     * @return string
+     */
+    public static function getImgUrl()
+    {
+        return self::getStaticUrl() . 'img/';
+    }
 }
--- www-oop/config/base.php
+++ www-oop/config/base.php
@@ -2,66 +2,12 @@
 
 // Store all config values here
 $base_config = array(
-	'css_path'         => getCssUrl(),
-	'js_path'          => getJsUrl(),
-	'img_path'         => getImgUrl(),
+	'css_path'         => SysHelper::getCssUrl(),
+	'js_path'          => SysHelper::getJsUrl(),
+	'img_path'         => SysHelper::getImgUrl(),
 	'points_css_class' => array(
 		0 => 'danger',
 		1 => 'warning',
 		3 => 'success'
 	)
 );
-
-/**
- * Get base host url
- *
- * @return string
- */
-function getHomeUrl() {
-	return 'http://' . $_SERVER['HTTP_HOST'];
-}
-
-/**
- * Get full site url
- *
- * @return string
- */
-function getSiteUrl() {
-    return getHomeUrl() . $_SERVER['REQUEST_URI'];
-}
-
-/**
- * Get static dir url
- *
- * @return string
- */
-function getStaticUrl() {
-	return getHomeUrl() . '/static/';
-}
-
-/**
- * Get css dir url
- *
- * @return string
- */
-function getCssUrl() {
-	return getStaticUrl() . 'css/';
-}
-
-/**
- * Get js dir url
- *
- * @return string
- */
-function getJsUrl() {
-	return getStaticUrl() . 'js/';
-}
-
-/**
- * Get img dir url
- *
- * @return string
- */
-function getImgUrl() {
-	return getStaticUrl() . 'img/';
-}
