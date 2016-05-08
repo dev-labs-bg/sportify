c997ff35338d9abce1a966e56bc7a32d8b28857c
Cvetan Mihaylov <cvetan.mihaylov@gmail.com>
2016-05-09 00:13:48 +0300

    add Tournaments controller logic for loading the data for the view

--- www-oop/application/tournamentsController.class.php
+++ www-oop/application/tournamentsController.class.php
@@ -2,10 +2,54 @@
 
 namespace Devlabs\App;
 
+/**
+ * Class TournamentsController
+ * @package Devlabs\App
+ */
 class TournamentsController extends AbstractController
 {
+    /**
+     * Default action method for rendering the tournaments page logic
+     *
+     * @return view
+     */
     public function index()
     {
-        return 'Tournaments Index';
+        /**
+         * Data array for keeping the variables which will be passed to the view
+         */
+        $data = array();
+
+        /**
+         * Load the data for the current user into an object
+         */
+        $user = new User();
+        $user->loadByEmail($_SESSION['email']);
+
+        /**
+         * Create object for holding the tournaments data
+         */
+        $tournaments = new TournamentCollection();
+
+        /**
+         * Check if there is a POST tournaments array set,
+         * and depending on the form submitted, join or leave tournaments
+         */
+        if (isset($_POST['tournaments'])) {
+            if (SysHelper::isFormSubmitted('tournaments_join')) {
+                $tournaments->join($user, $_POST['tournaments']);
+            } else if (SysHelper::isFormSubmitted('tournaments_leave')) {
+                $tournaments->leave($user, $_POST['tournaments']);
+            }
+        }
+
+        /**
+         * Store the joined and available tournaments data
+         * in the data array which will be passed to the view
+         */
+        $data['tournaments_joined'] = $tournaments->getJoined($user);
+        $data['tournaments_available'] = $tournaments->getAvailable($user);
+
+        return new view($this->view, $data);
     }
 }
