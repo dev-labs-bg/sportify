bd87ab1e2e93f64bb54976163337936efc563b11
Cvetan Mihaylov <cvetan.mihaylov@gmail.com>
2016-05-08 20:24:30 +0300

    add Collection class getByTournament() and getByUser() methods

--- www-oop/application/Score.class.php
+++ www-oop/application/Score.class.php
@@ -6,13 +6,15 @@ class Score
 {
     public $id;
     public $userId;
+    public $email;
     public $tournamentId;
     public $points;
 
-    public function __construct($id, $user_id, $tournament_id, $points)
+    public function __construct($id, $user_id, $email, $tournament_id, $points)
     {
         $this->id = $id;
         $this->userId = $user_id;
+        $this->email = $email;
         $this->tournamentId = $tournament_id;
         $this->points = $points;
     }
--- www-oop/application/ScoreCollection.class.php
+++ www-oop/application/ScoreCollection.class.php
@@ -4,5 +4,48 @@ namespace Devlabs\App;
 
 class ScoreCollection
 {
+    public $table = array();
 
+    public function getByTournament(Tournament $tournament)
+    {
+        $this->table = array();
+
+        $query = $GLOBALS['db']->query(
+            "SELECT users.email, scores.id, scores.user_id, scores.tournament_id, scores.points
+                FROM scores
+                INNER JOIN users ON users.id = scores.user_id
+                WHERE scores.tournament_id = :tournament_id
+                ORDER BY scores.points DESC, users.email ASC",
+            array('tournament_id' => $tournament->id)
+        );
+
+        if ($query) {
+            foreach ($query as $row) {
+                $this->table[] = new Score($row['id'], $row['user_id'], $row['email'], $row['tournament_id'], $row['points']);
+            }
+        }
+
+        return $this->table;
+    }
+
+    public function getByUser(User $user)
+    {
+        $this->table = array();
+
+        $query = $GLOBALS['db']->query(
+            "SELECT users.email, scores.id, scores.user_id, scores.tournament_id, scores.points
+                FROM scores
+                INNER JOIN users ON users.id = scores.user_id
+                WHERE scores.user_id = :user_id",
+            array('user_id' => $user->id)
+        );
+
+        if ($query) {
+            foreach ($query as $row) {
+                $this->table[] = new Score($row['id'], $row['user_id'], $row['email'], $row['tournament_id'], $row['points']);
+            }
+        }
+
+        return $this->table;
+    }
 }
