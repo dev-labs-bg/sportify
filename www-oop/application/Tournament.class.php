b82e89b031c19ad822e95c615a868b8cf8f3a375
Cvetan Mihaylov <cvetan.mihaylov@gmail.com>
2016-05-08 19:23:17 +0300

    add loadbyId() and loadbyName() methods to Tournament class

--- www-oop/application/Tournament.class.php
+++ www-oop/application/Tournament.class.php
@@ -8,7 +8,7 @@ class Tournament
     public $name;
     public $startDate;
     public $endDate;
-    public $seleted;
+    public $seleted = '';
 
     public function __construct($id, $name, $start, $end, $selected = '')
     {
@@ -17,5 +17,35 @@ class Tournament
         $this->startDate = $start;
         $this->endDate = $end;
         $this->seleted = $selected;
+    }
+
+    public function loadById($tournament_id)
+    {
+        $query = $GLOBALS['db']->query(
+            "SELECT * FROM tournaments WHERE id = :tournament_id",
+            array('tournament_id' => $tournament_id)
+        );
+
+        if ($query) {
+            $this->id = $query[0]['id'];
+            $this->name = $query[0]['name'];
+            $this->startDate = $query[0]['start'];
+            $this->endDate = $query[0]['end'];
+        }
+    }
+
+    public function loadByName($tournament_name)
+    {
+        $query = $GLOBALS['db']->query(
+            "SELECT * FROM tournaments WHERE name = :tournament_name",
+            array('tournament_name' => $tournament_name)
+        );
+
+        if ($query) {
+            $this->id = $query[0]['id'];
+            $this->name = $query[0]['name'];
+            $this->startDate = $query[0]['start'];
+            $this->endDate = $query[0]['end'];
+        }
     }
 }
