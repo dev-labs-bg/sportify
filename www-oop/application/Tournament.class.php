dc8134003e43c5715741b04e127e094fc1d36fcb
Cvetan Mihaylov <cvetan.mihaylov@gmail.com>
2016-05-08 18:28:44 +0300

    add TournamentsCollection class getJoined() and getAvailable() methods

--- www-oop/application/Tournament.class.php
+++ www-oop/application/Tournament.class.php
@@ -8,12 +8,14 @@ class Tournament
     public $name;
     public $startDate;
     public $endDate;
+    public $seleted;
 
-    public function __construct($id, $name, $start, $end)
+    public function __construct($id, $name, $start, $end, $selected = '')
     {
         $this->id = $id;
         $this->name = $name;
         $this->startDate = $start;
         $this->endDate = $end;
+        $this->seleted = $selected;
     }
 }
--- www-oop/application/TournamentsCollection.class.php
+++ www-oop/application/TournamentsCollection.class.php
@@ -4,5 +4,69 @@ namespace Devlabs\App;
 
 class TournamentsCollection
 {
+    public $items = array();
 
+    public function getJoined(User $user, $tournament_id = "ALL")
+    {
+        $this->items = array();
+
+        $query = $GLOBALS['db']->query(
+            "SELECT tournaments.id, tournaments.name, tournaments.start, tournaments.end
+                FROM scores
+                INNER JOIN users ON users.id = scores.user_id
+                INNER JOIN tournaments ON tournaments.id = scores.tournament_id
+                WHERE users.email = :email",
+            array('email' => $user->email)
+        );
+
+        if ($query) {
+            // set selected flag for selected tournament
+            foreach ($query as &$row) {
+                if ( $row['id'] == $tournament_id ) {
+                    $row['selected'] = 'selected';
+                } else {
+                    $row['selected'] = '';
+                }
+
+                $this->items[] = new Tournament($row['id'], $row['name'], $row['start'], $row['end'], $row['selected']);
+            }
+
+            return $this->items;
+        }
+
+        return $this->items;
+    }
+
+    public function getAvailable(User $user, $tournament_id = "ALL")
+    {
+        $this->items = array();
+
+        $query = $GLOBALS['db']->query(
+            "SELECT tournaments.id, tournaments.name, tournaments.start, tournaments.end
+                FROM tournaments
+                WHERE tournaments.id NOT IN
+                    (SELECT scores.tournament_id
+                    FROM scores
+                    INNER JOIN users ON users.id = scores.user_id
+                    WHERE users.email = :email)",
+            array('email' => $user->email)
+        );
+
+        if ($query) {
+            // set selected flag for selected tournament
+            foreach ($query as &$row) {
+                if ( $row['id'] == $tournament_id ) {
+                    $row['selected'] = 'selected';
+                } else {
+                    $row['selected'] = '';
+                }
+
+                $this->items[] = new Tournament($row['id'], $row['name'], $row['start'], $row['end'], $row['selected']);
+            }
+
+            return $this->items;
+        }
+
+        return $this->items;
+    }
 }
