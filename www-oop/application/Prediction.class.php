6ae752b9a151ac038da79925e545c3997b04710e
Cvetan Mihaylov <cvetan.mihaylov@gmail.com>
2016-05-09 18:51:41 +0300

    add Prediction class properties and construct() method

--- www-oop/application/Prediction.class.php
+++ www-oop/application/Prediction.class.php
@@ -4,5 +4,23 @@ namespace Devlabs\App;
 
 class Prediction
 {
+    public $id;
+    public $matchId;
+    public $userId;
+    public $homeGoals;
+    public $awayGoals;
+    public $points;
+    public $disabled = '';
 
+    public function __construct($id = '', $matchId = '', $userId = '',
+                                $homeGoals = '', $awayGoals = '', $points = '', $disabled = '')
+    {
+        $this->id = $id;
+        $this->matchId = $matchId;
+        $this->userId = $userId;
+        $this->homeGoals = $homeGoals;
+        $this->awayGoals = $awayGoals;
+        $this->points = $points;
+        $this->disabled = $disabled;
+    }
 }
