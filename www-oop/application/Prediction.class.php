2e0bc90baf03960e592ab53e068ebdb30cfcd0b8
Cvetan Mihaylov <cvetan.mihaylov@gmail.com>
2016-05-09 20:07:34 +0300

    add Prediction class loadById() method

--- www-oop/application/Prediction.class.php
+++ www-oop/application/Prediction.class.php
@@ -2,6 +2,10 @@
 
 namespace Devlabs\App;
 
+/**
+ * Class Prediction
+ * @package Devlabs\App
+ */
 class Prediction
 {
     public $id;
@@ -22,5 +26,38 @@ class Prediction
         $this->awayGoals = $awayGoals;
         $this->points = $points;
         $this->scoreAdded = $scoreAdded;
+    }
+
+    public function loadById(User $user, $id)
+    {
+        $query = $GLOBALS['db']->query(
+            "SELECT * FROM predictions WHERE match_id = :id AND user_id = :user_id",
+            array('id' => $id, 'user_id' => $user->id)
+        );
+
+        if ($query) {
+            $this->id = $query[0]['id'];
+            $this->matchId = $query[0]['match_id'];
+            $this->userId = $query[0]['user_id'];
+            $this->homeGoals = $query[0]['home_goals'];
+            $this->awayGoals = $query[0]['away_goals'];
+            $this->points = $query[0]['points'];
+            $this->scoreAdded = $query[0]['score_added'];
+        }
+    }
+
+    public function makePrediction(User $user, $matchId, $homeGoals, $awayGoals)
+    {
+
+    }
+
+    public function updatePrediction(User $user, $matchId, $homeGoals, $awayGoals)
+    {
+
+    }
+
+    public function insertPrediction(User $user, $matchId, $homeGoals, $awayGoals)
+    {
+
     }
 }
