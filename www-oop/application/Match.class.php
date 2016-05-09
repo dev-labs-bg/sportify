ce5a839a9e92fb0f5a10362ca78a953c8d980dca
Cvetan Mihaylov <cvetan.mihaylov@gmail.com>
2016-05-09 15:16:59 +0300

    add Match class construct() and loadById() methods

--- www-oop/application/Match.class.php
+++ www-oop/application/Match.class.php
@@ -4,5 +4,48 @@ namespace Devlabs\App;
 
 class Match
 {
+    public $id;
+    public $datetime;
+    public $homeTeam;
+    public $awayTeam;
+    public $homeGoals;
+    public $awayGoals;
+    public $tournamentId;
+    public $disabled = '';
 
+    public function __construct($id = '', $datetime = '', $homeTeam = '', $awayTeam = '',
+                                $homeGoals = '', $awayGoals = '', $tournamentId = '', $disabled = '')
+    {
+        $this->id = $id;
+        $this->datetime = $datetime;
+        $this->homeTeam = $homeTeam;
+        $this->awayTeam = $awayTeam;
+        $this->homeGoals = $homeGoals;
+        $this->awayGoals = $awayGoals;
+        $this->tournamentId = $tournamentId;
+        $this->disabled = $disabled;
+    }
+
+    /**
+     * Load a match's data from database by passing in its id
+     *
+     * @param $id
+     */
+    public function loadById($id)
+    {
+        $query = $GLOBALS['db']->query(
+            "SELECT * FROM matches WHERE id = :id",
+            array('id' => $id)
+        );
+
+        if ($query) {
+            $this->id = $query[0]['id'];
+            $this->datetime = $query[0]['datetime'];
+            $this->homeTeam = $query[0]['home_team'];
+            $this->awayTeam = $query[0]['away_team'];
+            $this->homeGoals = $query[0]['home_goals'];
+            $this->awayGoals = $query[0]['away_goals'];
+            $this->tournamentId = $query[0]['tournament_id'];
+        }
+    }
 }
