24ba0312f147711bb9929544bd00406606f6c0b7
Boryana <boryana@devlabs.bg>
2016-06-01 13:54:38 +0300

    Added: signup screen

---
+++ www/front-end/signup.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+    <title>Sportify</title>
+    <link rel="stylesheet" type="text/css" href="../bower_components/bootstrap/dist/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" type="text/css" href="../bower_components/bootstrap/dist/css/bootstrap.min.css">
+    <link href='https://fonts.googleapis.com/css?family=Exo:400,600,700,900' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+    <header>
+    <nav class="navbar navbar-default">
+        <h1 class="text-center sportify">Sportify</h1>
+        </nav>
+    </header>
+    <div class="container content signup-screen">
+        <div class="row">
+            <div class="col-sm-6 col-sm-offset-3 text-center">
+                <h2>Sign up</h2>
+                <form class="signup-form">
+                    <div class="form-group">
+                        <input name="email" type="email" class="form-control" placeholder="Email address">
+                    </div>
+                    <div class="form-group">
+                        <input name="first-name" type="text" class="form-control" placeholder="First Name">
+                    </div>
+                    <div class="form-group">
+                        <input name="last-name" type="text" class="form-control" placeholder="Last Name">
+                    </div>
+                    <div class="form-group">
+                        <input name="password" type="password" class="form-control" placeholder="Password">
+                    </div>
+                    <div class="form-group">
+                        <input name="confirm-password" type="password" class="form-control" placeholder="Confirm Password">
+                    </div>
+                    <button type="submit" class="btn btn-default green-btn">Confirm</button>
+                </form>
+            </div>
+        </div>
+    </div>
+<script src="../bower_components/jquery/dist/jquery.min.js"></script>
+<script src="../bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="js/script.js"></script>
+</body>
+</html>
