b3d10fcf12f3b321def7e6f1620d13234d31d607
Cvetan Mihaylov <cvetan.mihaylov@gmail.com>
2016-06-01 14:37:16 +0300

    Added: Scorec class getter and setter methods and ScoreRepository class

---
+++ www-symfony/src/Devlabs/SportifyBundle/Entity/ScoreRepository.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Devlabs\SportifyBundle\Entity;
+
+/**
+ * Class ScoreRepository
+ * @package Devlabs\SportifyBundle\Entity
+ */
+class ScoreRepository extends \Doctrine\ORM\EntityRepository
+{
+}
