356b5738679a938d900eccc9d7c2428847e1fbdc
Cvetan Mihaylov <cvetan.mihaylov@gmail.com>
2016-06-04 16:54:39 +0300

    Added: forms creation logic in the controller and in the TWIG template

--- www-symfony/src/Devlabs/SportifyBundle/Controller/MatchesController.php
+++ www-symfony/src/Devlabs/SportifyBundle/Controller/MatchesController.php
@@ -3,10 +3,12 @@
 namespace Devlabs\SportifyBundle\Controller;
 
 use Devlabs\SportifyBundle\Entity\Score;
+use Devlabs\SportifyBundle\Entity\Prediction;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 /**
@@ -35,6 +37,35 @@ class MatchesController extends Controller
         $forms = array();
 
         if ($matches) {
+            foreach ($matches as $match) {
+                $prediction = new Prediction();
+                $prediction->setHomeGoals('');
+                $prediction->setAwayGoals('');
+                $buttonAction = 'bet';
+
+                if (isset($predictions[$match->getId()])) {
+                    $prediction = $predictions[$match->getId()];
+                    $buttonAction = 'edit';
+                }
+
+                $formData = array();
+                $form = $this->createFormBuilder($formData)
+                    ->add('match_id', HiddenType::class, array('data' => $match->getId()))
+                    ->add('home_goals', TextType::class, array('data' => $prediction->getHomeGoals()))
+                    ->add('away_goals', TextType::class, array('data' => $prediction->getAwayGoals()))
+                    ->add('action', HiddenType::class, array('data' => $buttonAction))
+                    ->add('button', SubmitType::class, array('label' => $buttonAction))
+                    ->getForm();
+
+                $form->handleRequest($request);
+                $forms[$match->getId()] = $form;
+            }
+
+
+            // create view for each matches' form
+            foreach ($matches as $match) {
+                $forms[$match->getId()] = $forms[$match->getId()]->createView();
+            }
         }
 
         // rendering the view and returning the response
