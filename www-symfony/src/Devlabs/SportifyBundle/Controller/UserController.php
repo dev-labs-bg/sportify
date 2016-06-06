6ee93d925e48d18950f2b780d8afe16b6a46f11e
Jordan Enev <jordantoen@gmail.com>
2016-06-07 02:36:09 +0300

    Update user profile - username and password (with validations) is done.

--- www-symfony/src/Devlabs/SportifyBundle/Controller/UserController.php
+++ www-symfony/src/Devlabs/SportifyBundle/Controller/UserController.php
@@ -4,6 +4,7 @@ namespace Devlabs\SportifyBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 use Devlabs\SportifyBundle\Form\UserType;
 
 class UserController extends Controller
@@ -11,13 +12,27 @@ class UserController extends Controller
 	/**
 	 * @Route("/user/profile", name="user_profile")
 	 */
-	public function profileAction()
+	public function profileAction(Request $request)
 	{
 		$user = $this->getUser();
+
 		$form = $this->createForm(UserType::class, $user, array(
-			'action' => $this->generateUrl('user_update'),
+			'action' => $this->generateUrl('user_profile'),
 			'method' => 'POST',
 		));
+
+		$form->handleRequest($request);
+
+		if ($form->isValid()) {
+			$userManager = $this->container->get('fos_user.user_manager');
+			$user->setPlainPassword($form->get('password')->getData());
+			$userManager->updateUser($user, true);
+
+			$this->addFlash(
+				'notice',
+				'Your profile was updated successfully!'
+			);
+		}
 
 		return $this->render(
 			'DevlabsSportifyBundle:User:profile.html.twig',
--- www-symfony/src/Devlabs/SportifyBundle/Form/UserType.php
+++ www-symfony/src/Devlabs/SportifyBundle/Form/UserType.php
@@ -7,6 +7,8 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Security\Core\Validator\Constraints\UserPassword;
 
 class UserType extends AbstractType
 {
@@ -25,6 +27,19 @@ class UserType extends AbstractType
     {
         $builder
             ->add('username')
+            ->add('password_confirm', PasswordType::class, array(
+                'constraints' => new UserPassword(),
+                'mapped' => false
+            ))
+            ->add('password', RepeatedType::class, array(
+                'mapped' => false,
+                'type' => PasswordType::class,
+                'invalid_message' => 'The password fields must match.',
+                'options' => array('attr' => array('class' => 'password-field')),
+                'required' => true,
+                'first_options'  => array('label' => 'Password'),
+                'second_options' => array('label' => 'Repeat Password'),
+            ))
             ->add('save', SubmitType::class)
         ;
     }
