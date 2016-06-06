1481232273136a24895b09b21825b66c8f6f13bf
Cvetan Mihaylov <cvetan.mihaylov@gmail.com>
2016-06-06 11:21:58 +0300

    Added: Validation constraints to Prediction homeGoals, awayGoals

--- www-symfony/src/Devlabs/SportifyBundle/Entity/Prediction.php
+++ www-symfony/src/Devlabs/SportifyBundle/Entity/Prediction.php
@@ -3,6 +3,7 @@
 namespace Devlabs\SportifyBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="Devlabs\SportifyBundle\Entity\PredictionRepository")
@@ -33,11 +34,19 @@ class Prediction
 
     /**
      * @ORM\Column(type="integer", name="home_goals")
+     * @Assert\Type(
+     *     type="integer",
+     *     message="The value {{ value }} is not a valid {{ type }}."
+     * )
      */
     private $homeGoals;
 
     /**
      * @ORM\Column(type="integer", name="away_goals")
+     * @Assert\Type(
+     *     type="integer",
+     *     message="The value {{ value }} is not a valid {{ type }}."
+     * )
      */
     private $awayGoals;
 
