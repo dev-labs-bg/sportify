a59d108aa68748907a726351d426054339563414
Cvetan Mihaylov <cvetan.mihaylov@gmail.com>
2016-08-05 19:12:13 +0300

    Add updateFixtures() and updateMatchScores() methods to the DataUpdates Manager service

--- www-symfony/src/Devlabs/SportifyBundle/Controller/DefaultController.php
+++ www-symfony/src/Devlabs/SportifyBundle/Controller/DefaultController.php
@@ -12,7 +12,7 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
-        $footballAPI = $this->get('app.football_data.api');
+        $footballAPI = $this->get('app.data_updates.fetchers.football_data');
         $footballAPI->setApiToken('896fa7a2adc1473ba474c6eb4e66cb4c');
         $data = $footballAPI->fetchFixturesByTournamentAndMatchDay(426,1);
 //        $data = $footballAPI->fetchTeamsByTournament(426)->teams[17];
--- www-symfony/src/Devlabs/SportifyBundle/Services/DataUpdates/Manager.php
+++ www-symfony/src/Devlabs/SportifyBundle/Services/DataUpdates/Manager.php
@@ -2,10 +2,41 @@
 
 namespace Devlabs\SportifyBundle\Services\DataUpdates;
 
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
 /**
  * Class Manager
  * @package Devlabs\SportifyBundle\Services\DataUpdates
  */
 class Manager
 {
+    use ContainerAwareTrait;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    public function updateFixtures()
+    {
+        $dataFetcher = $this->container->get('app.data_updates.fetchers.football_data');
+        $dataFetcher->setApiToken('896fa7a2adc1473ba474c6eb4e66cb4c');
+
+        // get all tournaments from DB
+
+        // iterate the following actions for each tournament:
+
+        $dataFetched = $dataFetcher->fetchFixturesByTournamentAndDateRange($tournamentId, $dateFrom, $dateTo);
+
+        // invoke the parser service
+        // parse the fetched data
+
+        // invoke the importer service
+        // import the parsed data
+    }
+
+    public function updateMatchScores()
+    {
+    }
 }
