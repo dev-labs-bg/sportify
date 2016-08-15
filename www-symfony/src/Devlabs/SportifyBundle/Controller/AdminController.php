52ca0dd01ff5e23dfab7358bc43938f6813c9117
Cvetan Mihaylov <cvetan.mihaylov@gmail.com>
2016-08-15 17:40:41 +0300

    Create Admin controller syn_type form and View template

--- www-symfony/src/Devlabs/SportifyBundle/Controller/AdminController.php
+++ www-symfony/src/Devlabs/SportifyBundle/Controller/AdminController.php
@@ -4,14 +4,16 @@ namespace Devlabs\SportifyBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Devlabs\SportifyBundle\Entity\Tournament;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class AdminController extends Controller
 {
     /**
      * @Route("/admin", name="admin_index")
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
         // if user is not logged in, redirect to login page
         $securityContext = $this->container->get('security.authorization_checker');
@@ -38,17 +40,61 @@ class AdminController extends Controller
 
 //        $dataUpdatesManager->updateTeamsByTournament($tournament);
 
-        // set dateFrom and dateTo to respectively today and 1 week on
-//        $dateFrom = date("Y-m-d");
-//        $dateTo = date("Y-m-d", time() + 604800);
+        $formData = array();
 
-        // set dateFrom and dateTo to respectively yesterday and today
-        $dateFrom = date("Y-m-d", time() - 86400);
-        $dateTo = date("Y-m-d");
+        $form = $this->createFormBuilder($formData)
+            ->add('sync_type', ChoiceType::class, array(
+                'choices'  => array(
+                    'Next 7 days' => 'fixtures-next7days',
+                    'Past 1 day' => 'fixtures-past1day'
+                )))
+            ->add('button', SubmitType::class, array('label' => 'Select'))
+            ->getForm();
 
-        $dataUpdatesManager->updateFixtures($dateFrom, $dateTo);
-//        $dataUpdatesManager->updateFixtures('2016-07-09', '2016-07-10');
+        $form->handleRequest($request);
 
-        return $this->redirectToRoute('home');
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+
+            if ($data['sync_type'] === 'fixtures-next7days') {
+                // set dateFrom and dateTo to respectively today and 1 week on
+                $dateFrom = date("Y-m-d");
+                $dateTo = date("Y-m-d", time() + 604800);
+
+                $slackText = 'Match fixtures updated for next 7 days.';
+            } else if ($data['sync_type'] === 'fixtures-past1day') {
+                // set dateFrom and dateTo to respectively yesterday and today
+                $dateFrom = date("Y-m-d", time() - 86400);
+                $dateTo = date("Y-m-d");
+
+                $slackText = 'Match results updated for past 1 day.';
+            }
+
+            $dataUpdatesManager->updateFixtures($dateFrom, $dateTo);
+
+            // Get instance of the Slack service and send notification
+            $slack = $this->get('app.slack');
+            $slack->setUrl('https://hooks.slack.com/services/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');
+            $slack->setChannel('@ceco');
+            $slack->setText('<@ceco>: '.$slackText);
+            $slack->post();
+
+            return $this->redirectToRoute('admin_index');
+        }
+
+        $form = $form->createView();
+
+        // get the user's tournaments position data
+        $userScores = $em->getRepository('DevlabsSportifyBundle:Score')
+            ->getByUser($user);
+        $this->container->get('twig')->addGlobal('user_scores', $userScores);
+
+        // rendering the view and returning the response
+        return $this->render(
+            'Admin/index.html.twig',
+            array(
+                'form' => $form
+            )
+        );
     }
 }
