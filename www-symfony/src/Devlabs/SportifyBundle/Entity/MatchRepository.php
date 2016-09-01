6a648b2e3118e6e13f1471961e58da4c361f1a07
Cvetan Mihaylov <cvetan.mihaylov@gmail.com>
2016-09-01 16:32:13 +0300

    Fix logic Matches and History pages to display matches sorted by datetime FIRST and then by tournament

--- www-symfony/src/Devlabs/SportifyBundle/Entity/MatchRepository.php
+++ www-symfony/src/Devlabs/SportifyBundle/Entity/MatchRepository.php
@@ -29,8 +29,8 @@ class MatchRepository extends \Doctrine\ORM\EntityRepository
             ->where('p.scoreAdded IS NULL OR p.scoreAdded = 0')
             ->andWhere('m.homeGoals IS NULL OR m.awayGoals IS NULL')
             ->andWhere('m.datetime >= :date_from AND m.datetime <= :date_to')
-            ->orderBy('m.tournamentId')
-            ->addOrderBy('m.datetime')
+            ->orderBy('m.datetime')
+            ->addOrderBy('m.tournamentId')
             ->addOrderBy('tm.name')
             ->setParameters(array(
                 'user_id' => $user->getId(),
@@ -68,8 +68,8 @@ class MatchRepository extends \Doctrine\ORM\EntityRepository
             ->where('p.scoreAdded = 1 OR p.id IS NULL')
             ->andWhere('m.homeGoals IS NOT NULL AND m.awayGoals IS NOT NULL')
             ->andWhere('m.datetime >= :date_from AND m.datetime <= :date_to')
-            ->orderBy('m.tournamentId')
-            ->addOrderBy('m.datetime', 'DESC')
+            ->orderBy('m.datetime', 'DESC')
+            ->addOrderBy('m.tournamentId')
             ->addOrderBy('tm.name')
             ->setParameters(array(
                 'user_id' => $user->getId(),
