5618dc187434d8e45771b392c3460e087002996d
Cvetan Mihaylov <cvetan.mihaylov@gmail.com>
2017-02-09 13:53:31 +0200

    Add processForm() method to BaseApiController class

--- src/Devlabs/SportifyBundle/Controller/Base/BaseApiController.php
+++ src/Devlabs/SportifyBundle/Controller/Base/BaseApiController.php
@@ -5,6 +5,8 @@ namespace Devlabs\SportifyBundle\Controller\Base;
 use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\Controller\Annotations\View as ViewAnnotation;
 use FOS\RestBundle\Routing\ClassResourceInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
 
 /**
  * Class BaseApiController
@@ -12,7 +14,10 @@ use FOS\RestBundle\Routing\ClassResourceInterface;
  */
 abstract class BaseApiController extends FOSRestController implements ClassResourceInterface
 {
+    protected $entityName = 'Model';
+    protected $fqEntityClass = Model::class;
     protected $repositoryName = 'DevlabsSportifyBundle:Model';
+    protected $fqEntityFormClass = ModelType::class;
 
     /**
      * @ViewAnnotation(serializerEnableMaxDepthChecks=true)
@@ -38,5 +43,37 @@ abstract class BaseApiController extends FOSRestController implements ClassResou
             ->findOneById($id);
 
         return $object;
+    }
+
+    /**
+     * Create and process Entity form used for POST, PUT, PATCH requests
+     *
+     * @param Request $request
+     * @param ObjectManager $em
+     * @param $object
+     * @param $method
+     * @return \Symfony\Component\Form\FormErrorIterator
+     */
+    protected function processForm(Request $request, ObjectManager $em, $object, $method)
+    {
+        $form = $this->createForm(
+            $this->fqEntityFormClass,
+            $object,
+            array(
+                'csrf_protection' => false,
+                'method' => $method
+            )
+        );
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em->persist($object);
+            $em->flush();
+
+            return $object;
+        }
+
+        return $form->getErrors();
     }
 }
---
+++ src/Devlabs/SportifyBundle/Form/Api/UserType.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cmihaylov
+ * Date: 2/7/17
+ * Time: 4:57 PM
+ */
